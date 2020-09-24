fc5658a1a5f773e1bb8293055cfc03b7738be35e
Maria Byler <mariabyler98@gmail.com>
2020-09-24 15:19:29 -0400

    basic html added

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// array of objects
+var questions = [
+{
+    ques: "1+1",
+    options: ["1", "2", "3", "4"],  
+    answer: "2",
+}, 
+{
+    ques: "1+1" ,
+    options: ["1", "2", "3", "4"],  
+    answer: "2",
+}, 
+// last question
+{
+    ques: "1+1",
+    options: [1, 2, 3, 4],  
+    answer: 2,
+}
+] 
