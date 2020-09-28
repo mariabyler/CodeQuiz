a6801bd5edf93a2727d208fdb9f5fc7062752876
Maria Byler <mariabyler98@gmail.com>
2020-09-28 19:48:16 -0400

    added dynamically generated html to replace main div for first question

--- javascript.js
+++ javascript.js
@@ -1,14 +1,33 @@
+// start button function called
 var start = document.getElementById("start").addEventListener("click", startBtn); 
+// timer set
 var seconds = 60;  
 
+// function that runs when you click start button 
 function startBtn() {
-    // document.querySelector("main-div").style.visibility = "hidden"; 
-    setInterval(function() {
-        document.getElementById("timer").innerHTML = "time: " + seconds--;
-        }, 1000);
+    // replaces content of main div with dynamically generated html 
+    document.getElementById("header").innerHTML = "Commonly used data types DO NOT include:"; 
+    document.getElementById("main-content").innerHTML = "";
+    // function to remove start button 
+    function removeStartBtn() {
+        var elem = document.getElementById("start");
+        elem.parentNode.removeChild(elem);
+        return false;
+    }
+    // starts timer, if/then statement to keep timer from restarting every time start button is clicked
+    var element= document.querySelector("#start") 
+        if (element.classList.contains("active")) {
+        } 
+        else {
+         setInterval(function() {
+            document.getElementById("timer").innerHTML = "time: " + seconds--;
+            }, 1000);
+        document.getElementById("start").classList.add("active"); 
+    } 
+    // function to remove start button called
+    removeStartBtn(); 
 } 
 
-// there's a bug where, if you hit start twice, the clock counts down by two seconds
 console.log(startBtn); 
 
 // array of objects
